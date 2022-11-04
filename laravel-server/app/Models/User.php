ecca294cc370ec3dfe1eb89d20817d37971a6563
MJoe111 <joemoussally111@gmail.com>
2022-11-04 14:27:38 +0200

    Updating User Model

--- laravel-server/app/Models/User.php
+++ laravel-server/app/Models/User.php
@@ -12,6 +12,36 @@ class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
+        /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
     /**
      * Get the identifier that will be stored in the subject claim of the JWT.
      *
