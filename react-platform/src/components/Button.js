753e337c6b6cc17df4a6851d68e5ac0bd8e00117
MJoe111 <joemoussally111@gmail.com>
2022-11-05 17:35:20 +0200

    Adding outlined property to Button component

--- react-platform/src/components/Button.js
+++ react-platform/src/components/Button.js
@@ -2,11 +2,21 @@ import React from 'react'
 
 function Button({
     text,
+    onClick=() => {},
+    outlined=false,
     ...rest
 }) {
   return (
     <div
-        className='text-white bg-[#317bd6] w-fit py-2 px-4 rounded-md'
+        onClick={onClick}
+        className={
+            `hover:cursor-pointer w-fit py-2 px-4 rounded-md transition-all duration-300
+            ${
+                !outlined?'text-[#3d94ff] border-[#3d94ff] border-[1.6px] font-bold'
+                :'text-white bg-[#317bd6] hover:bg-[#275da1] font-semibold'
+            }
+            `
+        }
         style={
             rest.style?
             {...rest.style}
