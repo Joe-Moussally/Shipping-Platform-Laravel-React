5a8814bc7959195e4f76042671f65b08fb56c176
MJoe111 <joemoussally111@gmail.com>
2022-11-05 17:11:57 +0200

    Updating input field component

--- react-platform/src/components/InputField.js
+++ react-platform/src/components/InputField.js
@@ -1,15 +1,19 @@
 import React from 'react'
 
 function InputField({
+    id,
     label=null,
     placeholder=null,
     type,
+    value=null,
+    onChange,
+    onBlur=null,
     ...rest
 }) {
   return (
     
     // input component container
-    <div className='flex flex-col min-w-[400px] w-[70%] max-w-[612px]'>
+    <div className='flex flex-col my-6 min-w-[400px] w-[70%] max-w-[612px]'>
         {/* if input label is specified -> display it */}
         {
             label?
@@ -19,9 +23,13 @@ function InputField({
 
         {/* actual input field */}
         <input
+            id={id}
             placeholder={placeholder?placeholder:''}
             type={type}
-            className='p-2 bg-[#102540] text-gray-300 placeholder:text-gray-400 rounded-md border-[1.4px] border-gray-400 focus:outline-none focus:border-gray-200'
+            value={value?value:''}
+            onChange={onChange?onChange:() => {}}
+            onBlur={onBlur?onBlur:() => {}}
+            className='p-2 bg-[#102540] text-gray-300 placeholder:text-gray-400 rounded-md border-[1.4px] border-gray-400 focus:outline-none focus:border-gray-200 transition-all duration-300'
         />
     </div>
   )
--- react-platform/src/pages/SignUpPage.js
+++ react-platform/src/pages/SignUpPage.js
@@ -1,17 +1,63 @@
 import React from 'react'
+
+//packages
+import { useFormik } from 'formik';
+
+//components
 import InputField from '../components/InputField'
 
 function SignUpPage() {
+
+    //formik hook
+    const formik = useFormik({
+        initialValues:{
+            email: "",
+            password: "",
+            confirmPassword: ""
+        }
+    })
+
   return (
     <div className='bg-[#001024] h-[100vh] flex flex-col items-center'>
+
+        {/* Page title */}
         <span className='text-white text-6xl font-bold m-10'>Sign Up</span>
 
         <form autoComplete='off'>
+
+            {/* Email input */}
             <InputField
+                id='email'
                 label='Email'
                 type='email'
+                value={formik.values.email}
+                onChange={formik.handleChange}
                 placeholder='example@mail.com'
             />
+
+            {/* Password input */}
+            <InputField
+                id='password'
+                label='Password'
+                type='password'
+                placeholder='Password'
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+            />
+
+            {/* Confirm Password Input */}
+            <InputField
+                id='confirmPassword'
+                label='Confirm Password'
+                type='password'
+                placeholder='Confirm Password'
+                value={formik.values.confirmPassword}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+            />
+
+            {/* Submit button */}
         </form>
     </div>
   )
